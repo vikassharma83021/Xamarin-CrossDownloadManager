88383b2caaa94ce9f8a17356fd42b6bbccda282c
Simon Schick <simonsimcity@gmail.com>
2016-03-03 17:04:27 +0100

    Android: Implemented a task to update the DownloadFile instance [fixed #3]

--- CrossDownloadManager.Android/AndroidDownloadManager.cs
+++ CrossDownloadManager.Android/AndroidDownloadManager.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Android.App;
+using Android.Database;
 using Android.Content;
 
 namespace CrossDownloadManager.Android
 {
     public class AndroidDownloadManager : ICrossDownloadManager
     {
+        Android.OS.Handler _downloadWatcherHandler;
+        Java.Lang.Runnable _downloadWatcherHandlerRunnable;
+
         DownloadManager _downloadManager;
 
         public ObservableCollection<ICrossDownloadFile> Queue { get; private set; }
@@ -19,6 +24,9 @@ namespace CrossDownloadManager.Android
             Queue = new ObservableCollection<ICrossDownloadFile> ();
 
             _downloadManager = (DownloadManager)applicationContext.GetSystemService (Context.DownloadService);
+
+            // Check sequentially if parameters for any of the registered downloads changed
+            StartDownloadWatcher ();
         }
 
         public ICrossDownloadFile CreateDownloadFile (string url, IDictionary<string, string> headers)
@@ -45,8 +53,97 @@ namespace CrossDownloadManager.Android
 
         public void AbortAll ()
         {
-            foreach (var file in Queue) {
+            foreach (var file in Queue.ToList()) {
                 Abort (file);
+            }
+        }
+
+        void LoopOnDownloads (Action<ICursor> runnable)
+        {
+            // Reinitialize downloads that were started before the app was terminated or suspended
+            var query = new DownloadManager.Query ();
+            query.SetFilterByStatus (
+                Android.App.DownloadStatus.Paused |
+                Android.App.DownloadStatus.Pending |
+                Android.App.DownloadStatus.Running
+            );
+
+            try {
+                using (var cursor = _downloadManager.InvokeQuery (query)) {
+                    while (cursor.MoveToNext ()) {
+                        runnable.Invoke (cursor);
+                    }
+                }
+            } catch (Android.Database.Sqlite.SQLiteException) {
+                // I lately got an exception that the database was unaccessible ...
+            }
+        }
+
+        void StartDownloadWatcher ()
+        {
+            // Create an instance for a runnable-handler
+            _downloadWatcherHandler = new Android.OS.Handler ();
+
+            // Create a runnable, restarting itself to update every file in the queue
+            _downloadWatcherHandlerRunnable = new Java.Lang.Runnable (() => {
+
+                // Loop throught all files in the system-queue and update the data in the local queue
+                LoopOnDownloads(cursor => UpdateFileProperties (cursor));
+
+                var nextExecution = (Queue.Count == 0) ? 1000 : 100;
+                _downloadWatcherHandler.PostDelayed (_downloadWatcherHandlerRunnable, nextExecution);
+            });
+
+            // Start this playing handler immediately
+            _downloadWatcherHandler.PostDelayed (_downloadWatcherHandlerRunnable, 0);
+        }
+
+        public void UpdateFileProperties (ICursor cursor)
+        {
+            int id = cursor.GetInt (cursor.GetColumnIndex (DownloadManager.ColumnId));
+            var downloadFile = Queue.Cast<AndroidDownloadFile> ().FirstOrDefault (f => f.Id == id);
+
+            if (downloadFile != null) {
+                UpdateFileProperties (cursor, downloadFile);
+            }
+        }
+
+        /**
+         * Update the properties for a file by it's cursor.
+         * This method should be called in an interval and on reinitialization.
+         */
+        public void UpdateFileProperties (ICursor cursor, AndroidDownloadFile downloadFile)
+        {
+            downloadFile.TotalBytesWritten = cursor.GetInt (cursor.GetColumnIndex (DownloadManager.ColumnBytesDownloadedSoFar));
+            downloadFile.TotalBytesExpected = cursor.GetInt (cursor.GetColumnIndex (DownloadManager.ColumnTotalSizeBytes));
+
+            switch (cursor.GetInt (cursor.GetColumnIndex (DownloadManager.ColumnStatus))) {
+                // Successful
+                case 8:
+                downloadFile.Status = DownloadStatus.COMPLETED;
+                Queue.Remove (downloadFile);
+                break;
+
+                // Failed
+                case 16:
+                downloadFile.Status = DownloadStatus.FAILED;
+                Queue.Remove (downloadFile);
+                break;
+
+                // Paused
+                case 4:
+                downloadFile.Status = DownloadStatus.PAUSED;
+                break;
+
+                // Pending
+                case 1:
+                downloadFile.Status = DownloadStatus.PENDING;
+                break;
+
+                // Running
+                case 2:
+                downloadFile.Status = DownloadStatus.RUNNING;
+                break;
             }
         }
     }
--- CrossDownloadManager.Android/DownloadCompletedBroadcastReceiver.cs
+++ CrossDownloadManager.Android/DownloadCompletedBroadcastReceiver.cs
@@ -15,52 +15,21 @@ namespace CrossDownloadManager.Android
 
         public override void OnReceive (Context context, Intent intent)
         {
-            var downloadQueue = _downloadManager.Queue.Cast<AndroidDownloadFile> ().ToList ();
-
             long reference = intent.GetLongExtra (DownloadManager.ExtraDownloadId, -1);
 
-            var downloadFile = downloadQueue.FirstOrDefault (f => f.Id == reference);
+            var downloadFile = _downloadManager.Queue.Cast<AndroidDownloadFile> ().FirstOrDefault (f => f.Id == reference);
             if (downloadFile != null) {
                 var query = new DownloadManager.Query ();
-                query.SetFilterById (reference);
+                query.SetFilterById (downloadFile.Id);
 
-                var cursor = ((DownloadManager)context.GetSystemService (Context.DownloadService)).InvokeQuery (query);
-                cursor.MoveToFirst ();
-
-                int bytesDownloaded = cursor.GetInt (cursor.GetColumnIndex (DownloadManager.ColumnBytesDownloadedSoFar));
-                int status = cursor.GetInt (cursor.GetColumnIndex (DownloadManager.ColumnStatus));
-                int totalSizeBytes = cursor.GetInt (cursor.GetColumnIndex (DownloadManager.ColumnTotalSizeBytes));
-
-                downloadFile.TotalBytesWritten = bytesDownloaded;
-                downloadFile.TotalBytesExpected = totalSizeBytes;
-
-                switch (status) {
-                // Successful
-                case 8:
-                    downloadFile.Status = DownloadStatus.COMPLETED;
-                    _downloadManager.Queue.Remove (downloadFile);
-                    break;
-
-                // Failed
-                case 16:
-                    downloadFile.Status = DownloadStatus.FAILED;
-                    _downloadManager.Queue.Remove (downloadFile);
-                    break;
-
-                // Paused
-                case 4:
-                    downloadFile.Status = DownloadStatus.PAUSED;
-                    break;
-
-                // Pending
-                case 1:
-                    downloadFile.Status = DownloadStatus.PENDING;
-                    break;
-
-                // Running
-                case 2:
-                    downloadFile.Status = DownloadStatus.RUNNING;
-                    break;
+                try {
+                    using (var cursor = ((DownloadManager)context.GetSystemService (Context.DownloadService)).InvokeQuery (query)) {
+                        while (cursor.MoveToNext ()) {
+                            ((AndroidDownloadManager)_downloadManager).UpdateFileProperties (cursor, downloadFile);
+                        }
+                    }
+                } catch (Android.Database.Sqlite.SQLiteException) {
+                    // I lately got an exception that the database was unaccessible ...
                 }
             }
         }
