9646d98117a64154fe1f8ddac0cd93c241d10362
Simon Schick <simonsimcity@gmail.com>
2016-05-20 15:11:40 +0200

    Enabled Xamarin.Mac implementation (It just throws an exception, but it's possible to include it)

---
+++ DownloadManager/Plugin.DownloadManager.Mac/DownloadManagerImplementation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Plugin.DownloadManager.Abstractions;
+
+namespace Plugin.DownloadManager
+{
+    /// <summary>
+    /// The UWP implementation of the download manager.
+    /// </summary>
+    public class DownloadManagerImplementation : IDownloadManager
+    {
+        public ObservableCollection<IDownloadFile> Queue { get; private set; }
+
+        public Func<IDownloadFile, string> PathNameForDownloadedFile { get; set; }
+
+        public IDownloadFile CreateDownloadFile (string url, IDictionary<string, string> headers)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void Start (IDownloadFile i)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void Abort (IDownloadFile i)
+        {
+            throw new NotImplementedException ();
+        }
+
+        public void AbortAll ()
+        {
+            throw new NotImplementedException ();
+        }
+    }
+}
