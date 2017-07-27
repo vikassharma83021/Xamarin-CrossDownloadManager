b35568a1602d63822cf0fddbe92f68a5fe72e47f
Simon Schick <simonsimcity@gmail.com>
2017-07-27 10:16:29 +0400

    Fixed Android sample

--- Sample/Droid/MainApplication.cs
+++ Sample/Droid/MainApplication.cs
@@ -8,24 +8,10 @@ namespace DownloadExample.Droid
     [Application]
     public class MainApplication : Application
     {
-        Plugin.DownloadManager.ActivityLifecycleCallbacks _downloadManagerLifecycleCallbacks;
-
         public MainApplication(IntPtr handle, JniHandleOwnership transer)
           :base(handle, transer)
         {
-            _downloadManagerLifecycleCallbacks = new Plugin.DownloadManager.ActivityLifecycleCallbacks();
         }
 
-		public override void OnCreate()
-        {
-            base.OnCreate();
-            RegisterActivityLifecycleCallbacks(_downloadManagerLifecycleCallbacks);
-        }
-
-        public override void OnTerminate()
-        {
-            base.OnTerminate();
-            UnregisterActivityLifecycleCallbacks(_downloadManagerLifecycleCallbacks);
-        }
 	}
 }
